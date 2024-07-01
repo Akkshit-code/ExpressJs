4a4a65e9966873a7ad40179b80b0780fcd531d3e
Akkshit-code <akkshitsingh27@gmail.com>
2024-07-01 11:08:10 +0530

    Project setup for Backend

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// require('dotenv').config({path: './env'});
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+     path: './env'
+});
+
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// const app=exprss();
+
+// (async ()=> {
+//      try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_name}`)
+//         app.on("error",(error)=>{
+//           console.log("ERROR",error);
+//           throw error;
+//         });
+
+//         app.listen(process.env.PORT,()=>{
+//           console.log(`App is listening on p${process.env.PORT}`);
+//         })
+
+//      } catch(error){
+//           console.error("ERROR :",error)
+//           throw error;
+//      }
+// })()
